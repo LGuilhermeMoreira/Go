db9d866f3a6ed40ece596c433f86e7c2c81146d0
LGuilhermeMoreira <Lguilhermemoreiraleite@gmail.com>
2024-01-02 04:22:36 -0300

    conexão estabelecida | insertProduct criado

--- 4 - database/1/go.mod
+++ 4 - database/1/go.mod
@@ -2,4 +2,7 @@ module database/1
 
 go 1.21.5
 
-require github.com/google/uuid v1.5.0
+require (
+	github.com/go-sql-driver/mysql v1.7.1
+	github.com/google/uuid v1.5.0
+)
--- 4 - database/1/main.go
+++ 4 - database/1/main.go
@@ -1,7 +1,43 @@
 package main
 
-import "github.com/google/uuid"
+import (
+	// Usando a interface para o banco de dados.
+	"database/sql"
 
+	// Importando os drivers do banco de dados MySQL.
+	_ "github.com/go-sql-driver/mysql"
+
+	// Importando a biblioteca UUID do Google para gerar IDs únicos.
+	"github.com/google/uuid"
+)
+
+func main() {
+
+	//sql.Open("<banco de dados>","<user>:<password>@tcp(localhost:<porta>)/<nome do DB")
+	// Para fazer a conexão com o banco de dados MySQL.
+	db, err := sql.Open("mysql", "root:root@tcp(localhost:3306)/goexpert")
+
+	// Verifica se ocorreu algum erro ao abrir a conexão com o banco de dados.
+	if err != nil {
+		panic(err)
+	}
+
+	// Garante que a conexão com o banco de dados será fechada no final da função main.
+	defer db.Close()
+
+	// Cria uma instância de Product usando a função NewProduct, que gera um ID único.
+	product := NewProduct("IPHONE 15", 9000.0)
+
+	// Insere o produto no banco de dados.
+	err = insertProduct(db, product)
+
+	// Verifica se ocorreu algum erro durante a inserção do produto no banco de dados.
+	if err != nil {
+		panic(err)
+	}
+}
+
+// Definindo a estrutura do produto.
 type Product struct {
 	ID    string
 	Name  string
@@ -17,14 +53,39 @@ type Product struct {
 // 	}
 // }
 
+// Função construtora para criar uma nova instância de Product com um ID único gerado.
 func NewProduct(name string, price float64) *Product {
 	return &Product{
-		ID:    uuid.New().String(),
+		ID:    uuid.New().String(), // Gera um ID único usando a biblioteca UUID do Google.
 		Name:  name,
 		Price: price,
 	}
 }
 
-func main() {
+// Função para inserir um produto no banco de dados.
+func insertProduct(db *sql.DB, product *Product) error {
+	// Prepara a declaração SQL para a inserção do produto.
+	stmt, err := db.Prepare("INSERT INTO products(id, name, price) VALUES (?,?,?)")
 
+	// Verifica se ocorreu algum erro ao preparar a declaração SQL.
+	if err != nil {
+		return err
+	}
+
+	// Garante que a declaração SQL será fechada no final da função insertProduct.
+	defer stmt.Close()
+
+	// podemos pegar o result e checar algumas informações
+	// result, err = stmt.Exec(product.ID, product.Name, product.Price)
+
+	// Executa a declaração SQL para inserir o produto no banco de dados.
+	_, err = stmt.Exec(product.ID, product.Name, product.Price)
+
+	// Verifica se ocorreu algum erro durante a execução da declaração SQL.
+	if err != nil {
+		return err
+	}
+
+	// Retorna nil, indicando que a operação foi bem-sucedida.
+	return nil
 }
